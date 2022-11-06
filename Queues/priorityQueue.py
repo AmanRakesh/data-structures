7dc89f6550c4d967fe98067db56f7494a7180a9c
Aman Rakesh <amanrakesh046@gmail.com>
2022-11-06 23:53:50 +0530

    queue corrections and init for priority queue

---
+++ Queues/priorityQueue.py
@@ -0,0 +1,14 @@
+class Node:
+
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+        self.priority = None
+
+class PriorityQueue:
+
+    def __init__(self):
+        head = None
+        tail = None
+        size = 0
+        
