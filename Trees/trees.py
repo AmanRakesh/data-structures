b0ee294290b77be3baf027213a6c2b68d286cbcf
Aman Rakesh <amanrakesh046@gmail.com>
2022-11-10 22:19:58 +0530

    trees init

---
+++ Trees/trees.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(self, value):
+        self.next = None
+        self.value = value
+
+class Trees:
+
+    def __init__(self):
+        left = None
+        right = None
