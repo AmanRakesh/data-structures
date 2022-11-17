fba340bd471dae04fd969ceabd19c1be76c03139
Aman Rakesh <amanrakesh046@gmail.com>
2022-11-17 23:31:38 +0530

    binary tree rewrite

--- Trees/binary_trees.py
+++ Trees/binary_trees.py
@@ -1,35 +1,17 @@
-class Node:
-
+class BinaryTrees:
+    
     def __init__(self, value):
+        self.value = value
         self.left = None
         self.right = None
-        self.value = value
-
-class BinaryTrees:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, value):
-        if self.root == None:
-            self.root == Node(value)
-
-        else:
-            root = self.root
-            
-            #value is less hence adding in left
-            if root.value <= value:
-                temp = root.left
-                if temp == None:
-                    temp = Node(value)
-                    root.left = temp
-                
-                else:
-                    self.insert(value)
 
 
-    def search(self):
-        pass
+def inorderTraversal(temp):
+    if temp == None:
+        return
+        
+    inorderTraversal(temp.left)
+    print(temp.value)
+    inorderTraversal(temp.right)
 
-    def inorderTraversal(self):
-        pass
 
