8d513e732171bb11b855852c49133f60385e10c0
Aman Rakesh <amanrakesh046@gmail.com>
2022-11-26 14:05:12 +0530

    binary tree traversal added

--- Trees/binary_trees.py
+++ Trees/binary_trees.py
@@ -6,6 +6,15 @@ class BinaryTrees:
         self.right = None
 
 
+
+# Binary Tree Traversals:
+# Tree Traversal algorithms can be classified broadly into two categories:
+
+# Depth-First Search (DFS) Algorithms
+# Breadth-First Search (BFS) Algorithms
+
+
+# below are dfs traversal methods
 def inorderTraversal(temp):
     if temp == None:
         return
@@ -13,5 +22,27 @@ def inorderTraversal(temp):
     inorderTraversal(temp.left)
     print(temp.value)
     inorderTraversal(temp.right)
+
+
+def preorderTraversal(temp):
+
+    if temp == None:
+        return
+
+    print(temp.value)
+    inorderTraversal(temp.left)
+    inorderTraversal(temp.right)
+
+def postOrderTraversal(temp):
+    
+    if temp == None:
+        return
+
+    inorderTraversal(temp.left)
+    inorderTraversal(temp.right)
+    print(temp.value)
+
+    
+
 
 
